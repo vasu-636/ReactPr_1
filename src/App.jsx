73dcd6b2f1ad3a1b94140c653ae36202c132d833
divyadarshan007 <dasdivyadarshan@gmail.com>
2025-10-16 11:06:36 +0530

    nabar completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './assets/Components/header'
+
+const App = () => {
+  return (
+   <section>
+    <Header /> 
+   </section>
+  )
+}
+
+export default App
